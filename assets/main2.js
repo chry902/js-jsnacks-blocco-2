30f4a8f94efc7039f84a4efe0857dd04bc14d29a
chry902 <tchry902@gmail.com>
2021-10-25 12:17:54 +0200

    esercizio1

---
+++ assets/main2.js
@@ -0,0 +1,4 @@
+/***Descrizione:
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0. */
