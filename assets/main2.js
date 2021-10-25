a2435fbcd0b7d2b78065e862a2ff114d8ecb7de1
chry902 <tchry902@gmail.com>
2021-10-25 12:25:03 +0200

    es2

--- assets/main2.js
+++ assets/main2.js
@@ -2,3 +2,33 @@
 Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
 Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0. */
+
+
+const squadre = [
+    {
+
+        juve: `cannondale`,
+        punti_fatti: 10,
+        falli_subiti: 3
+    },
+    {
+
+        milan: `cannondale1`,
+        punti_fatti: 12,
+        falli_subiti: 5
+    },
+    {
+
+        inter: `cannondale2`,
+        punti_fatti: 8,
+        falli_subiti: 1
+    },
+    {
+
+        napoli: `cannondale3`,
+        punti_fatti: 7,
+        falli_subiti: 4
+    },
+
+
+]
