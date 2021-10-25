50ed6e18ce200cccbeb4423254fce12670a39a81
chry902 <tchry902@gmail.com>
2021-10-25 12:18:09 +0200

    es1

--- assets/main1.js
+++ assets/main1.js
@@ -1,16 +1,37 @@
-const bici =
-    [
-        {
-        `nome`: `montainBike`
-            `peso`: 12 ;
-    };
-{
-    `nome`: `bmx`
-            `peso`: 20;
-};
-{
-    `nome`: `corsa`
-            `peso`: 8;
-};
+const bicycles = [
+    {
 
-];
+        name: `cannondale`,
+        weight: 10
+    },
+    {
+
+        name: `cannondale1`,
+        weight: 12
+    },
+    {
+
+        name: `cannondale2`,
+        weight: 8
+    },
+    {
+
+        name: `cannondale3`,
+        weight: 7
+    },
+
+
+]
+
+let lightestBike = bicycles[0]
+
+for (let i = 0; i < bicycles.length; i++) {
+    const element = bicycles[i];
+
+    if (lightestBike.weight < lightestBike.weight) {
+        lightestBike = bike
+    };
+
+}
+
+document.querySelector(`.risultato`).innerHTML = `la bici piu leggera è : ${lightestBike.name} e pesa : ${lightestBike.weight} kg`
