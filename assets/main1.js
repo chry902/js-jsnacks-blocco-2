5b456060cdde8d2c792ceaac6a60b09f8bc027aa
chry902 <tchry902@gmail.com>
2021-10-25 11:35:55 +0200

    esercizio 1

---
+++ assets/main1.js
@@ -0,0 +1,16 @@
+const bici =
+    [
+        {
+        `nome`: `montainBike`
+            `peso`: 12 ;
+    };
+{
+    `nome`: `bmx`
+            `peso`: 20;
+};
+{
+    `nome`: `corsa`
+            `peso`: 8;
+};
+
+];
